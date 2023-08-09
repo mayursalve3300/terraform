4027bb60962c70724e4848223a30cfaebe58652d
root <root@ip-172-31-40-221.ap-south-1.compute.internal>
2023-08-09 13:24:25 +0000

    vpc creation

---
+++ vpc.tf
@@ -0,0 +1,78 @@
+provider "aws" {
+
+	access_key = "AKIAUHIRTM33MIXTXTJ3"
+	secret_key = "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"
+	region = "ap-south-1"
+}
+
+######Creating VPC#####
+
+resource "aws_vpc" "test" {
+
+	cidr_block = "10.10.0.0/16"
+	tags =  {
+	     Name = "test-vpc"
+	}
+}
+
+#####Creating SUBNETS ####
+
+resource "aws_subnet" "subnet-1" {
+
+	vpc_id = "${aws_vpc.test.id}"
+	cidr_block = "10.10.1.0/24"
+	availability_zone = "ap-south-1a"
+	tags =  {
+		Name = "subnet-1"
+	}
+}
+
+resource "aws_subnet" "subnet-2" {
+
+        vpc_id = "${aws_vpc.test.id}"
+        cidr_block = "10.10.2.0/24"
+        availability_zone = "ap-south-1b"
+        tags = {
+                Name = "subnet-2"
+        }
+}
+
+####### Creating Internet gateway ######
+
+resource "aws_internet_gateway" "igw" {
+
+	vpc_id = "${aws_vpc.test.id}"
+	
+
+}
+
+######### Route table#########
+
+resource "aws_route_table" "public-rt" {
+
+	vpc_id = "${aws_vpc.test.id}"
+	route {
+		cidr_block = "0.0.0.0/0"
+		gateway_id = "${aws_internet_gateway.igw.id}"
+
+	}
+}
+
+resource "aws_route_table_association" "public-routing" {
+
+	subnet_id = "${aws_subnet.subnet-1.id}"
+	route_table_id = "${aws_route_table.public-rt.id}"
+}
+
+
+
+############ luanch instance in subnet 1########
+
+resource "aws_instance" "test-1" {
+
+	ami = "ami-098dd3a86ea110896"
+	instance_type = "t2.micro"
+	subnet_id = "${aws_subnet.subnet-1.id}"
+
+}
+
